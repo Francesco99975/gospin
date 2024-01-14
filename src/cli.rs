872ecc5b8213233b5a9c632ddb4bd0ad0d352e27
Francesco Michele Barranca <francescomich99@gmail.com>
2024-01-14 14:48:16 -0500

    updated description

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(author = "Francesco Michele Barranca (kalairendev)", version = "1.0", about = "Gospin - Project Initiator", long_about = None)]
+#[command(author = "Francesco Michele Barranca (kalairendev)", version = "0.1.5", about = "Gospin - GO + HTMX Boilerplate Initiator", long_about = None)]
 #[command(propagate_version = true)]
 pub struct Gospin {
     pub project: Option<String>,
